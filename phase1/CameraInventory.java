5f38a77adc153f9ed9fe08d6f7724c7b5b13a9e2
SubbaReddy <91176313+Subbutechie@users.noreply.github.com>
2023-05-20 17:32:28 +0530

    After Sprint2: Final Draft

--- phase1/CameraInventory.java
+++ phase1/CameraInventory.java
@@ -19,33 +19,79 @@ public class CameraInventory {
         System.out.print("Enter the rent amount of the camera: ");
         double rentAmount = scanner.nextDouble();
         String Status = "AVAILABLE";
-        System.out.println(Status);
 
         Camera camera = new Camera( brand, model, rentAmount, Status);
         hashmap.put(nextCameraId, camera);
         nextCameraId++;
 
-        System.out.println("Camera added successfully!");
+        System.out.println("Camera added successfully to the list");
+        System.out.println("------NEXT-------");
     }
     public void removeCamera(int cameraId) {
         Camera removedCamera = hashmap.remove(cameraId);
         if (removedCamera != null) {
-            System.out.println("Camera with ID " + cameraId + " has been removed.");
+            System.out.println("Camera with ID " + cameraId + " has been removed from the list.");
+            System.out.println("-----------------------------------------------------------------");
         } else {
             System.out.println("Camera with ID " + cameraId + " does not exist.");
+            System.out.println("-------------------------------------------------");
         }
     }
 
     public void displayCameras() {
         System.out.println("Camera Inventory:");
-        for (HashMap.Entry<Integer, Camera> entry : hashmap.entrySet()) {
-            int cameraId = entry.getKey();
-            Camera camera = entry.getValue();
-            System.out.println("Camera ID: " + cameraId);
-            System.out.println("Brand: " + camera.getBrand());
-            System.out.println("Model: " + camera.getModel());
-            System.out.println("Rent Amount: " + camera.getRentAmount());
-            System.out.println("Status: " + camera.getStatus());
-            System.out.println("---------------");
+        System.out.println("=========================================================================================");
+            System.out.printf("%-10s %-15s %-20s %-15s %s%n", "Camera ID", "Brand", "Model", "Rent Amount", "Status");
+            System.out.println("=====================================================================================");
+            for (HashMap.Entry<Integer, Camera> entry : hashmap.entrySet()) {
+                int cameraId = entry.getKey();
+                Camera camera = entry.getValue();
+                System.out.printf("%-10d %-15s %-20s %-15.2f %s%n",
+                        cameraId, camera.getBrand(), camera.getModel(), camera.getRentAmount(), camera.getStatus());
+                System.out.println("-----------------------------------------------------------------------------");
+            }
+
+     }
+        public void mycameras() {
+            System.out.println("List of my cameras");
+            System.out.println("====================================================================================");
+        	System.out.printf("%-10s %-15s %-20s %-15s %s%n", "Camera ID", "Brand", "Model", "Rent Amount", "Status");
+        	System.out.println("=====================================================================================");
+            for (HashMap.Entry<Integer, Camera> my : hashmap.entrySet()) {
+                int cameraId = my.getKey();
+                Camera camera = my.getValue();
+                if (camera.getStatus().equalsIgnoreCase("rented")) {
+                	System.out.printf("%-10d %-15s %-20s %-15.2f %s%n",
+                             cameraId, camera.getBrand(), camera.getModel(), camera.getRentAmount(), camera.getStatus());
+                	System.out.println("----------------------------------------------------------------------------");
+                }
+            }
         }
-}  }  
+
+        	
+        public void rentCamera(int cameraId, double walletBalance) {
+            Camera camera = hashmap.get(cameraId);
+            if (camera != null) {
+                if (camera.getStatus().equalsIgnoreCase("Available")) {
+                    double rentAmount = camera.getRentAmount();
+                    if (walletBalance >= rentAmount) {
+                        camera.setStatus("Rented");
+                        System.out.println("Camera with ID " + cameraId + " has been rented.");
+                        walletBalance -= rentAmount;
+                        System.out.println("Wallet balance after renting: " + walletBalance);
+                    } else {
+                        System.out.println("error: Transaction failed due to Insufficient balance. Please Deposit the amount to the wallet.");
+                    }
+                } else {
+                    System.out.println("Camera with ID " + cameraId + " is not available for rent.");
+                }
+            } else {
+                System.out.println("Camera with ID " + cameraId + " does not exist.");
+            }
+        }
+
+    	
+    	
+    	
+    
+}  
--- phase1/Main.java
+++ phase1/Main.java
@@ -9,26 +9,50 @@ public class Main {
 		System.out.println("=================================");
 		System.out.println("|   WELCOME TO CAMERA RENTAL.IO |");
 		System.out.println("=================================");
+		System.out.println("                            By SubbaReddy PV");
 		System.out.println("PLEASE LOGIN TO CONTINUE -");
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Username: ");
-		String un = sc.nextLine();
 		System.out.println("Password: ");
 		String p = sc.nextLine();
+		// press ENTER to continue
+		
 		HashMap<Integer,Camera> hashmap= new HashMap<>();
 
-			Camera camera1 = new Camera("Sony","Alpha A7",2500,"Available");
-			Camera camera2 = new Camera("Canon","EOS 5D",3000,"Available");
-			Camera camera3 = new Camera("Nikon","D850",3500,"Available");
-			Camera camera4 = new Camera("GoPro","Hero 9",4000,"Rented");
-			Camera camera5 = new Camera("Fujifilm","X-24",4500,"Available");
+			Camera camera1 = new Camera("Sony","Alpha A7",2500,"AVAILABLE");
+			Camera camera2 = new Camera("Canon","EOS 5D",3000,"RENTED");
+			Camera camera3 = new Camera("Nikon","D850",3500,"AVAILABLE");
+			Camera camera4 = new Camera("GoPro","Hero 9",4000,"RENTED");
+			Camera camera5 = new Camera("Chroma","CT",1000,"RENTED");
+			Camera camera6 = new Camera("Fujifilm","X-24",4500,"AVAILABLE");
+			Camera camera7 = new Camera("Panasonic Lumix","GH5",2500,"AVAILABLE");
+			Camera camera8 = new Camera("Leica","Q2",1800,"RENTED");
+			Camera camera9 = new Camera("DJI Mavic ","2 Pro",1500,"AVAILABLE");
+			Camera camera10 = new Camera("Olympus","OM-D E-M1 Mark III",4200,"AVAILABLE");
+			Camera camera11 = new Camera("DJI","Mavic 2 Pro",6500,"AVAILABLE");
+			Camera camera12 = new Camera("Sony","Cyber-shot RX100 VII",5500,"RENTED");
+			Camera camera13 = new Camera("Olympus","PEN-F",8700,"AVAILABLE");
+			Camera camera14 = new Camera("DJI","Osmo Pocket",4300,"AVAILABLE");
+			Camera camera15 = new Camera("GoPro","Hero8 Black",5700,"AVAILABLE");
 			
 			hashmap.put(1, camera1);
 			hashmap.put(2, camera2);
 			hashmap.put(3, camera3);
 			hashmap.put(4, camera4);
 			hashmap.put(5, camera5);
+			hashmap.put(6, camera6);
+			hashmap.put(7, camera7);
+			hashmap.put(8, camera8);
+			hashmap.put(9, camera9);
+			hashmap.put(10, camera10);
+			hashmap.put(11, camera11);
+			hashmap.put(12, camera12);
+			hashmap.put(13, camera13);
+			hashmap.put(14, camera14);
+			hashmap.put(15, camera15);
+			
+			
 			System.out.println("----------MENU------------");
 			System.out.println("1. MY CAMERA");
 			System.out.println("2. RENT A CAMERA");
@@ -41,6 +65,7 @@ public class Main {
 		
 		while(running) {
 			int choice = sc.nextInt();
+			double walletBalance = 10000;
 			
 			switch(choice) {
 			case 1:
@@ -54,22 +79,23 @@ public class Main {
 				while(submenu != 4) {
 					switch(submenu) {
 					case 1: 
-						
 						op.addCamera();
 						break;
-					case 2:
 						
-						System.out.println("Enter the camera ID: ");
+					case 2:
+						System.out.println("Enter the camera ID to remove: ");
 						int key  = sc.nextInt();
 						op.removeCamera(key);
 						break;
-					case 3:
 						
-						op.displayCameras();
+					case 3:
+						op.mycameras();
 						break;
+						
 					case 4:
 						System.out.println("Going to previous menu");
 						break;
+						
 					default:
 						System.out.println("Invalid choice");
 					}
@@ -88,8 +114,54 @@ public class Main {
     			System.out.println("5. EXIT");
 				break;
 			case 2:
-				
-				
+				CameraInventory disp = new CameraInventory(hashmap);
+				disp.displayCameras();
+				System.out.println("Enter camera ID to rent");
+				int cameraId = sc.nextInt();
+				disp.rentCamera( cameraId, walletBalance);
+				System.out.println("Continue your search");
+				System.out.println("----------MENU------------");
+    			System.out.println("1. MY CAMERA");
+    			System.out.println("2. RENT A CAMERA");
+    			System.out.println("3. VIEW ALL CAMERAS");
+    			System.out.println("4. MY WALLET");
+    			System.out.println("5. EXIT");
+				break;
+			
+			case 3:
+				CameraInventory show = new CameraInventory(hashmap);	
+				System.out.println("Inventory");
+				show.displayCameras();
+				System.out.println("Continue further");
+				System.out.println("----------MENU------------");
+    			System.out.println("1. MY CAMERA");
+    			System.out.println("2. RENT A CAMERA");
+    			System.out.println("3. VIEW ALL CAMERAS");
+    			System.out.println("4. MY WALLET");
+    			System.out.println("5. EXIT");
+				break;
+			case 4:
+				System.out.println("YOUR CURRENT WALLET BALANCE IS " +walletBalance);
+				System.out.println("Do you want to add balance to your wallet(1.YES/2.NO"));
+				int opt = sc.nextInt();
+				if(opt == 1) {
+					System.out.println("Enter the amount to add: ");
+					int deposit = sc.nextInt();
+					walletBalance += deposit;
+					System.out.println("YOUR WALLET BALANCE IS UPDATED SUCCESSFULLY");
+					System.out.println("CURRENT WALLET BALANCE IS:" +walletBalance);
+				}
+				System.out.println("Continue your search");
+				System.out.println("----------MENU------------");
+    			System.out.println("1. MY CAMERA");
+    			System.out.println("2. RENT A CAMERA");
+    			System.out.println("3. VIEW ALL CAMERAS");
+    			System.out.println("4. MY WALLET");
+    			System.out.println("5. EXIT");
+				break;
+			case 5:
+				running = false;
+				System.out.println("Thank you, Happy Filming!");
 			}
 		}
 				
@@ -124,4 +196,10 @@ class Camera {
     public String getStatus() {
     	return Status;
     }
+    public void setStatus(String Status) {
+    	this.Status = Status;
+    }
+
+
+	
 }
