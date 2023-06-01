032ea98c3ca296c045faa0da8e81e6c6e392bd1e
SubbaReddy <91176313+Subbutechie@users.noreply.github.com>
2023-06-01 14:33:14 +0530

    Lazycollection

--- EProduct.java
+++ EProduct.java
@@ -3,19 +3,31 @@ package com.ecommerce;
 import java.math.BigDecimal;
 import java.util.Date;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "eproduct")
 public class EProduct {
 
+	@Id
+	@GeneratedValue
+	@Column(name = "ID")
 	private long ID;
+
+	@Column(name = "name")
 	private String name;
+
+	@Column(name = "price")
 	private BigDecimal price;
+
+	@Column(name = "date_added")
 	private Date dateAdded;
-	
+
 	public EProduct() {
-		
+
 	}
-	
+
 	public EProduct(long id, String name, BigDecimal price, Date dateAdded) {
-		
 		this.ID = id;
 		this.name = name;
 		this.price = price;
@@ -53,7 +65,5 @@ public class EProduct {
 	public void setDateAdded(Date dateAdded) {
 		this.dateAdded = dateAdded;
 	}
-	
-	
-	
+
 }
--- HibernateQueryDemo.java
+++ HibernateQueryDemo.java
@@ -1,79 +1,48 @@
 package com;
 
-import java.io.IOException;
+import java.io.*;
 import java.io.PrintWriter;
-import java.util.Date;
 import java.util.List;
-
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import com.ecommerce.*;
-import java.math.*;
-import java.text.ParseException;
+import jakarta.servlet.http.*;
+
+import org.hibernate.*;
+
+import com.ecommerce.EProduct;
 
 @WebServlet("/query-demo")
 public class HibernateQueryDemo extends HttpServlet {
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>");
 
-        // Get the input parameters from the request
-        int id = Integer.parseInt(request.getParameter("id"));
-        String name = request.getParameter("name");
-        BigDecimal price = new BigDecimal(request.getParameter("price"));
-        // Assuming dateAdded is in the format of "yyyy-MM-dd"
-        String dateAddedString = request.getParameter("dateAdded");
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        Date dateAdded;
-        try {
-            dateAdded = dateFormat.parse(dateAddedString);
-        } catch (ParseException e) {
-            // Handle the exception if the date format is invalid
-            // You can send an error response or take appropriate action
-            e.printStackTrace();
-            return;
-        }
+		// STEP 1: Get a Session (connection) from the Session Factory class
+		SessionFactory factory = HibernateUtil.getSessionFactory();
 
-        // STEP 1: Get a Session (connection) from the Session Factory class
-        SessionFactory factory = HibernateUtil.getSessionFactory();
+		// STE2 Create the session object
+		Session session = factory.openSession();
 
-        // STEP 2: Create the session object
-        Session session = factory.openSession();
+		out.println("Hibernate Session opened.<br>");
 
-        out.println("Hibernate Session opened.<br>");
+		// STEP 3 Query the DB and get the data
+		List<EProduct> eproducts = session.createQuery("from EProduct").list();
+		out.println("<table border=1>");
+		for (EProduct prod : eproducts) {
+			out.println("<tr>" + "<td>" + prod.getID() + "<td>" + prod.getName() + "<td>" + prod.getPrice() + "<td>"
+			+ prod.getDateAdded());
+		}
 
-        EProduct details = new EProduct();
-        details.setID(id);
-        details.setName(name);
-        details.setPrice(price);
-        details.setDateAdded(dateAdded);
+		out.println("</table>");
 
-        // Save the EProduct object to the database
-        session.beginTransaction();
-        session.save(details);
-        session.getTransaction().commit();
+		session.close();
 
-        // Query the DB and get the data
-        List<EProduct> eproducts = session.createQuery("from EProduct").list();
-        out.println("<table border=1>");
-        for (EProduct prod : eproducts) {
-            out.println("<tr><td>" + prod.getID() + "<td>" + prod.getName() + "<td>" + prod.getPrice() + "<td>"
-                    + prod.getDateAdded());
-        }
-        out.println("</table>");
+		out.println("Hibernate Session closed.<br>");
 
-        session.close();
+		out.println("</body></html>");
+	}
 
-        out.println("Hibernate Session closed.<br>");
-
-        out.println("</body></html>");
-    }
 }
